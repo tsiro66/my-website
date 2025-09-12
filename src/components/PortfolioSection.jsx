5dd56f84df6a2cad5bd38ae57e6b7c695032dbf9
tsiro66 <116112100+tsiro66@users.noreply.github.com>
2025-09-12 17:31:22 +0300

    Minor changes

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -58,6 +58,7 @@ const PortfolioSection = ({ onContactClick }) => {
       });
     }, 100);
     return () => clearTimeout(timer);
+// eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const getProgressColor = (progress) => {
